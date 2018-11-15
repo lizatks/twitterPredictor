1f4f4b1e454e777608c03ed6a678aa6c29af99cc
Fanny <fanny.riclet@student-cs.fr>
2018-11-15 09:07:48 +0100

    Ajouts tuto API Twitter

--- twitterPredictor/hello_world.py
+++ twitterPredictor/hello_world.py
@@ -16,6 +16,24 @@ def twitter_setup():
     api = tweepy.API(auth)
     return api
 
+#recherche les tweets avec le mot macron, en langue française avec un max de 100 tweets retournés par page
+#Avec API.Search : ne considère les tweets que des 7 derniers jours
+def collect():
+    connexion = twitter_setup()
+    tweets = connexion.search("Macron",language="french",rpp=100)
+    for tweet in tweets:
+        print(tweet.text)
+
+collect()
 
 
+#recherche les tweets d'un utilisateur identifié par son id
+def collect_by_user(user_id):
+    connexion = twitter_setup()
+    statuses = connexion.user_timeline(id = user_id, count = 200)
+    for status in statuses:
+        print(status.text)
+    return statuses
+
+collect_by_user(322)
 
