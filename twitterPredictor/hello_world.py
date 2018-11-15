e3e3e76a4f8d27c5a9464943df28fadb91fdbc00
Fanny <fanny.riclet@student-cs.fr>
2018-11-15 09:13:12 +0100

    Ajout tuto streaming

--- twitterPredictor/hello_world.py
+++ twitterPredictor/hello_world.py
@@ -24,8 +24,6 @@ def collect():
     for tweet in tweets:
         print(tweet.text)
 
-collect()
-
 
 #recherche les tweets d'un utilisateur identifié par son id
 def collect_by_user(user_id):
@@ -35,5 +33,31 @@ def collect_by_user(user_id):
         print(status.text)
     return statuses
 
-collect_by_user(322)
+
+#recherche en streaming : interrompre = CTRL + C
+from tweepy.streaming import StreamListener
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        if  str(status) == "420":
+            print(status)
+            print("You exceed a limited number of attempts to connect to the streaming API")
+            return False
+        else:
+            return True
+
+
+
+
+def collect_by_streaming():
+
+    connexion = twitter_setup()
+    listener = StdOutListener()
+    stream=tweepy.Stream(auth = connexion.auth, listener=listener)
+    stream.filter(track=['Emmanuel Macron'])
+
 
