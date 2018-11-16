e2985349000b485d3a1c9f01aa98222c112e72ba
Fanny <fanny.riclet@student-cs.fr>
2018-11-16 11:48:46 +0100

    Ajout exception dans sentiments/ Fonction pour avoir avis sur candidat

--- twitterPredictor/tweet_analysis/Analyse Sentiments.py
+++ twitterPredictor/tweet_analysis/Analyse Sentiments.py
@@ -7,5 +7,17 @@ def sentiments(text,langue) :
     if langue == 'EN' :
         return(wiki.sentiment)
     else :
-        return(wiki.translate(to = 'EN').sentiment)
+        try :
+            return(wiki.translate(to = 'EN').sentiment)
+        except :
+            return(wiki.sentiment)
 
+def avis_sur_candidat(candidat, langue) :
+    data = collect_to_pandas_dataframe("@" + candidat)
+    tweets = data['tweet_textual_content']
+    n = len(tweets)
+    somme_polarity = 0
+    somme_subjectivity = 0
+    for tweet in tweets :
+        somme_polarity, somme_subjectivity = sentiments(tweet,langue)
+    return(somme_polarity/n, somme_subjectivity/n)
