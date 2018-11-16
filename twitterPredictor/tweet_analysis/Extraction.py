3e34bda6b032394905008b01e9ffab606e42ea2a
Fanny <fanny.riclet@student-cs.fr>
2018-11-16 09:56:02 +0100

    Ajout fonction max likes

--- twitterPredictor/tweet_analysis/Extraction.py
+++ twitterPredictor/tweet_analysis/Extraction.py
@@ -11,3 +11,13 @@ def  extract_max_rt(query) :
     print("Number of retweets: {}".format(rt_max))
     print("{} characters.\n".format(data['len'][rt]))
 
+
+def extract_max_likes(query) :
+    data = collect_to_pandas_dataframe(query)
+    like_max  = np.max(data['Likes'])
+    like  = data[data.RTs == like_max].index[0]
+
+    # Max Likes:
+    print("The tweet with more likes is: \n{}".format(data['tweet_textual_content'][like]))
+    print("Number of retweets: {}".format(like_max))
+    print("{} characters.\n".format(data['len'][like]))
