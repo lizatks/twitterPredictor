adcceb88a04be59ed4c05a2c66ca4567f6a3dffe
Fanny <fanny.riclet@student-cs.fr>
2018-11-16 10:25:40 +0100

    Commentaires sur les algos d'extraction

--- twitterPredictor/tweet_analysis/Extraction.py
+++ twitterPredictor/tweet_analysis/Extraction.py
@@ -1,7 +1,10 @@
 from twitterPredictor.twitter_collect.collect import *
 
 
-def  extract_max_rt(query) :
+def extract_max_rt(query) :
+    """Extrait le tweet avec le plus de RT d'un dataframe de tweets
+    :param query : requête utilisée dans l'api.search pour créer la dataframe de tweets
+    :return print le tweet avec le plus de RT"""
     data = collect_to_pandas_dataframe(query)
     rt_max  = np.max(data['RTs'])
     rt  = data[data.RTs == rt_max].index[0]
@@ -13,6 +16,9 @@ def  extract_max_rt(query) :
 
 
 def extract_max_likes(query) :
+    """Extrait le tweet avec le plus de Likes d'un dataframe de tweets
+    :param query : requête utilisée dans l'api.search pour créer la dataframe de tweets
+    :return print le tweet avec le plus de Likes"""
     data = collect_to_pandas_dataframe(query)
     like_max  = np.max(data['Likes'])
     like  = data[data.RTs == like_max].index[0]
@@ -24,5 +30,9 @@ def extract_max_likes(query) :
 
 
 
-def like_candidate(query) :
-    data = collect_to_pandas_dataframe(query)
+def moy_like_rt_candidate(candidate) :
+    """Calcule la moyenne de Likes et de RT sur les 200 derniers tweets d'un candidat
+    :param candidate : (str) nom du candidat à considérer (cf candidates_id)
+    :return : la moyenne des likes, la moyenne des RTs"""
+    data = collect_to_pandas_dataframe_user(candidate)
+    return(data['Likes'].mean(),data['RTs'].mean())
