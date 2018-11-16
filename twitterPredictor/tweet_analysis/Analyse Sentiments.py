487e18c6870077c1572912f7dd5c53677701fe6d
Fanny <fanny.riclet@student-cs.fr>
2018-11-16 11:49:54 +0100

    simplification fonction sentiments

--- twitterPredictor/tweet_analysis/Analyse Sentiments.py
+++ twitterPredictor/tweet_analysis/Analyse Sentiments.py
@@ -1,23 +1,20 @@
-def sentiments(text,langue) :
+def sentiments(text) :
     """renvoie les sentiments d'un texte
     :param text : (str) chaîne de caractère à analyser
     :param langue : (str) langue de text ('EN' pour English)
     :return les sentiments : la polarité et subjectivité du message"""
     wiki = TextBlob(text)
-    if langue == 'EN' :
+    try :
+        return(wiki.translate(to = 'EN').sentiment)
+    except :
         return(wiki.sentiment)
-    else :
-        try :
-            return(wiki.translate(to = 'EN').sentiment)
-        except :
-            return(wiki.sentiment)
 
-def avis_sur_candidat(candidat, langue) :
+def avis_sur_candidat(candidat) :
     data = collect_to_pandas_dataframe("@" + candidat)
     tweets = data['tweet_textual_content']
     n = len(tweets)
     somme_polarity = 0
     somme_subjectivity = 0
     for tweet in tweets :
-        somme_polarity, somme_subjectivity = sentiments(tweet,langue)
+        somme_polarity, somme_subjectivity = sentiments(tweet)
     return(somme_polarity/n, somme_subjectivity/n)
